ecc5397811b7fb04da774bb345e9ccae02c4537b
Matochichi <ttzw.3514taku.pla@gmail.com>
2023-11-07 01:07:41 +0900

    commit

--- slide-generator.jsx
+++ slide-generator.jsx
@@ -1,38 +1,22 @@
-// GUI関連
-var gUIWindow = new Window('dialog', 'Puller');
-gUIWindow.bounds = [200, 100, 580, 280];
+var gUIWindow = new Window('dialog','テストウィンドウ'); 
 
-gUIWindow.previewPanel = gUIWindow.add('panel', [10, 10, 370, 130]);
-gUIWindow.previewPanel.text = 'キャンバスプレビュー';
+gUIWindow.bounds = [200,100,580,280]; 
 
-gUIWindow.previewImage = gUIWindow.previewPanel.add('image', [10, 20, 360, 120], '');
+gUIWindow.activeDocPnl = gUIWindow.add("panel",[10,5,370,90],"アクティブドキュメント"); 
+gUIWindow.activeDocPnl .editText = gUIWindow.activeDocPnl .add("edittext",[10,15,345,33], ""); 
 
-gUIWindow.okBtn = gUIWindow.add('button', [80, 145, 175, 170], '実行');
-gUIWindow.okBtn.onClick = function() {
-    updateCanvasPreview();
-};
+gUIWindow.activeDocPnl .editText.enabled = false ;
 
-gUIWindow.cancelBtn = gUIWindow.add('button', [190, 145, 285, 170], 'キャンセル');
-gUIWindow.cancelBtn.onClick = function() {
-    gUIWindow.close();
-};
+gUIWindow.activeDocPnl .radiorBtn1 = gUIWindow.activeDocPnl .add("radiobutton",[10,40,200,58], "オリジナル"); 
+gUIWindow.activeDocPnl .radiorBtn2 = gUIWindow.activeDocPnl .add("radiobutton",[10,55,200,75], "複製"); 
 
-// メインスクリプト
+gUIWindow.activeDocPnl .radiorBtn1.value = true; 
 
-//プレビューの更新
-function updateCanvasPreview() {
-    var activeDoc = app.activeDocument;
+gUIWindow.dList = gUIWindow.add("dropdownlist",[15,110,365,130],["ぼかし（ガウス）20","ダスト＆スクラッチ20,10"]); 
+gUIWindow.dList.selection = 0; 
 
-    var tempFile = new File(Folder.temp + '/' + activeDoc.name + '.png');
-
-    var pngOptions = new PNGSaveOptions();
-    pngOptions.compression = 9;
-
-    activeDoc.saveAs(tempFile, pngOptions, true, Extension.LOWERCASE);
-
-    gUIWindow.previewImage.image = tempFile.fsName;
-
-    tempFile.remove();
-}
+gUIWindow.filterPnl = gUIWindow.add("panel",[10,90,370,140],"フィルターの処理"); 
+gUIWindow.okBtn = gUIWindow.add("button",[80,145,175,170], "実行"); 
+gUIWindow.cancelBtn = gUIWindow.add("button",[190,145,285,170], "キャンセル"); 
 
 gUIWindow.show();
